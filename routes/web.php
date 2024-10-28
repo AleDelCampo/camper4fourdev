aad3b03d099ebef5bcafab8a5dab30e8b6d37848
AleHashirama <alehashirama@gmail.com>
2024-10-28 19:56:40 +0100

    Redirect

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,8 @@ use App\Http\Controllers\TripController;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
 
 Route::get('/trips/create', [TripController::class, 'create'])->name('trips.create');
 Route::post('/trips', [TripController::class, 'store'])->name('trips.store');
