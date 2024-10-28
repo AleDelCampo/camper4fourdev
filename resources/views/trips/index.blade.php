53cfcf2efcfcb3a28eabc9781e386c5e1ceed787
AleHashirama <alehashirama@gmail.com>
2024-10-28 20:16:05 +0100

    Index

---
+++ resources/views/trips/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <h1>Lista dei Trips</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Data</th>
+                <th>Immagine</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($trips as $trip)
+            <tr>
+                <td>{{ $trip->id }}</td>
+                <td>{{ $trip->name }}</td>
+                <td>{{ $trip->date }}</td>
+                <td>
+                    <img src="{{ Storage::url($trip->image_path) }}" alt="Image" width="100">
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-
 Route::get('/trips/create', [TripController::class, 'create'])->name('trips.create');
 Route::post('/trips', [TripController::class, 'store'])->name('trips.store');
 Route::get('/trips', [TripController::class, 'index'])->name('trips.index');
