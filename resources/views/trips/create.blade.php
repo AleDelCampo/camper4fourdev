c8b61066863850fddad452d23ccf8ae3a617a43a
AleHashirama <alehashirama@gmail.com>
2024-10-29 18:46:08 +0100

    Building images

--- resources/views/trips/create.blade.php
+++ resources/views/trips/create.blade.php
@@ -1,24 +1,17 @@
 @extends('layouts.app')
 @section('content')
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Crea un Nuovo Elemento</title>
-</head>
-<body>
-    <h1>Crea un Nuovo Elemento</h1>
-    <form action="{{ route('trips.store') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <label for="name">Nome:</label>
-        <input type="text" name="name" required>
-        
-        <label for="date">Data:</label>
-        <input type="date" name="date" required>
-        
-        <label for="image">Immagine:</label>
-        <input type="file" name="image" accept="image/*" required>
-        
-        <button type="submit">Crea</button>
-    </form>
-</body>
+<h1>Crea un Nuovo Elemento</h1>
+<form action="{{ route('trips.store') }}" method="POST" enctype="multipart/form-data">
+    @csrf
+    <label for="name">Nome:</label>
+    <input type="text" name="name" required>
+
+    <label for="date">Data:</label>
+    <input type="date" name="date" required>
+
+    <label for="image">Immagine:</label>
+    <input type="file" name="image" accept="image/*" required>
+
+    <button type="submit">Crea</button>
+</form>
 @endsection
